d2c35a426cd9b921c141de8deeb5504a1ea06e50
Ghexd <army2013.ac@gmail.com>
2025-12-01 21:36:59 +0100

    Add Properties File
    Add LivelyProperties.json to allow user to customize some parameters

--- script.js
+++ script.js
@@ -13,6 +13,40 @@ const CONFIG = {
     SPLAT_TO_TRIANGLE: 2   
 };
 
+// Convert Hex color from Lively to RGB object
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+function livelyPropertyListener(name, val)
+{
+  switch(name) {
+    case "mainColor":
+      var color = hexToRgb(val);
+      if(color) {
+          CONFIG.baseColor = { r: color.r, g: color.g, b: color.b };
+          generateMesh(); 
+      }
+      break;
+      
+    case "blinkColor":
+      var color = hexToRgb(val);
+      if(color) {
+          CONFIG.brightColor = { r: color.r, g: color.g, b: color.b };
+      }
+      break;   
+      
+    case "trianglesNumber":
+      CONFIG.triangleCount = val;
+      generateMesh();
+      break;     
+  }
+}
 
 // Delaunay library
 var Delaunay;
